ffd608dd1ff3d280ccc8eca759d751a82f977bab
Kenneth Fernandez <kbfernandez@mymail.mines.edu>
2020-10-04 14:11:16 -0700

    Basic Setup through instructions

---
+++ src/experiment/TestBoard.java
@@ -0,0 +1,22 @@
+package experiment;
+
+import java.util.Set;
+
+public class TestBoard {
+
+	public TestBoard() {
+		// Sets up the board
+	}
+	
+	public void calcTargets(TestBoardCell startCell, int path) {
+		
+	}
+	
+	Set<TestBoardCell> getTargets(){
+		return null;
+	}
+
+	TestBoardCell getCell(int row, int col) {
+		return null;	
+	}
+}
---
+++ src/experiment/TestBoardCell.java
@@ -0,0 +1,32 @@
+package experiment;
+
+import java.util.Set;
+
+public class TestBoardCell {
+	private int row, column;
+	Set<TestBoardCell> getAdjList() {
+		return null;
+	}
+	
+	public TestBoardCell(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	public void setRoom(boolean check) {
+		
+	}
+	
+	public boolean isRoom() {
+		return false;
+	}
+	
+	public void setOccupied(boolean check) {
+		
+	}
+	
+	public boolean getOccupied() {
+		return false;
+	}
+
+}
