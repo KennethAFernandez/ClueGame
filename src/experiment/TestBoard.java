50e367d25002206d37a88a329e1e14528cb7d4a9
Kenneth Fernandez <kbfernandez@mymail.mines.edu>
2020-10-05 13:22:16 -0700

    Added a matrix setup tests, and calc targets test

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -1,21 +1,46 @@
 package experiment;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class TestBoard {
 	@SuppressWarnings("unused")
 	private Map<TestBoardCell, Set<TestBoardCell>> adjMtx;
-	
+	private List<TestBoardCell> visited = new ArrayList<TestBoardCell>();
+	private int path;
+
 	public TestBoard() {
 		adjMtx = new HashMap<TestBoardCell, Set<TestBoardCell>>();
 	}
-	
+
 	public void calcTargets(TestBoardCell startCell, int path) {
+		this.path = path;
+		for(TestBoardCell cell : startCell.getAdjList()) {
+			for(TestBoardCell visit : visited) {
+				if(visit == cell) {
+					break;
+				} else {
+					visited.add(cell);
+					if(path == 1) {
+						adjMtx.put(cell, startCell.getAdjList());
+					} else {
+						calcTargets(cell, path-1);
+					}
+				}
+				adjMtx.remove(cell);
+			}
+		}
+		adjMtx.put(startCell, startCell.getAdjList());
 	}
 	
+	public boolean getPath() {
+		return path <= 6;
+	}
+
 	public Set<TestBoardCell> getTargets(){ 
 		Set<TestBoardCell> test = new HashSet<TestBoardCell>();
 		return test;
--- src/tests/BoardTestsExp.java
+++ src/tests/BoardTestsExp.java
@@ -1,10 +1,8 @@
 package tests;
 import java.util.Set;
-
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
-
 import experiment.TestBoard;
 import experiment.TestBoardCell;
 
@@ -15,18 +13,55 @@ public class BoardTestsExp {
 	public void setUp() {
 		board = new TestBoard();
 	}
-
+	
 	@Test
-	public void testAdjacency() {
+	public void testAdjacency() { 
 		TestBoardCell cell = board.getCell(0,0);
 		Set<TestBoardCell> testList = cell.getAdjList();
 		Assert.assertTrue(testList.contains(board.getCell(1, 0)));
 		Assert.assertTrue(testList.contains(board.getCell(0, 1)));
 		Assert.assertEquals(2, testList.size());
 	}
-
 	@Test
-	public void testTargetsNormal() {
+	public void topLeftCorner() { 
+		TestBoardCell cell = board.getCell(0,0);
+		Set<TestBoardCell> testList = cell.getAdjList();
+		Assert.assertTrue(testList.contains(board.getCell(0, 0)));
+	}
+	@Test 
+	public void bottomRightCorner() { 
+		TestBoardCell cell = board.getCell(0,0);
+		Set<TestBoardCell> testList = cell.getAdjList();
+		Assert.assertTrue(testList.contains(board.getCell(3, 3)));
+	}
+	@Test
+	public void rightEdge() { 
+		TestBoardCell cell = board.getCell(0,3);
+		Set<TestBoardCell> testList = cell.getAdjList();
+		Assert.assertTrue(testList.contains(board.getCell(0, 2)));
+		Assert.assertTrue(testList.contains(board.getCell(1, 3)));
+	}
+	@Test
+	public void leftEdge() { 
+		TestBoardCell cell = board.getCell(3,0);
+		Set<TestBoardCell> testList = cell.getAdjList();
+		Assert.assertTrue(testList.contains(board.getCell(2, 0)));
+		Assert.assertTrue(testList.contains(board.getCell(3, 1)));
+	}
+	@Test
+	public void testMiddleSquareOfGrid() { 
+		TestBoardCell cell = board.getCell(0, 0);
+		Set<TestBoardCell> testList = cell.getAdjList();
+		Assert.assertTrue(testList.contains(board.getCell(1, 1)));
+		Assert.assertTrue(testList.contains(board.getCell(1, 2)));
+		Assert.assertTrue(testList.contains(board.getCell(2, 1)));
+		Assert.assertTrue(testList.contains(board.getCell(2, 2)));
+	}
+	
+	
+	
+	@Test
+	public void testTargetsNormal() { 
 		TestBoardCell cell = board.getCell(0, 0);
 		board.calcTargets(cell, 3);
 		Set<TestBoardCell> targets = board.getTargets();
@@ -38,7 +73,13 @@ public class BoardTestsExp {
 		Assert.assertTrue(targets.contains(board.getCell(0, 3)));
 		Assert.assertTrue(targets.contains(board.getCell(1, 0)));
 	}
-
+	@Test 
+	public void testOccupied() { 
+		TestBoardCell cell = board.getCell(0,0);
+		board.getCell(0, 1).setOccupied(true);
+		board.calcTargets(cell, 1);
+		Assert.assertEquals(true, board.getCell(0, 1).getOccupied());
+	}
 	@Test
 	public void testTargetsMixed() {
 		board.getCell(0, 2).setOccupied(true);
@@ -50,41 +91,19 @@ public class BoardTestsExp {
 		Assert.assertTrue(targets.contains(board.getCell(2, 2)));
 		Assert.assertTrue(targets.contains(board.getCell(3, 3)));
 	}
-	
 	@Test
-	public void topLeftCorner() {
-		TestBoardCell cell = board.getCell(0,0);
-		Set<TestBoardCell> testList = cell.getAdjList();
-		Assert.assertTrue(testList.contains(board.getCell(0, 0)));
+	public void noMoreThanSix() { 
+		TestBoardCell cell = board.getCell(0, 3);
+		board.calcTargets(cell, 7);
+		Assert.assertEquals(true, board.getPath());
 	}
-	
-	@Test 
-	public void bottomRightCorner() {
-		TestBoardCell cell = board.getCell(0,0);
-		Set<TestBoardCell> testList = cell.getAdjList();
-		Assert.assertTrue(testList.contains(board.getCell(3, 3)));
-	}
-	
 	@Test
-	public void rightEdge() {
-		TestBoardCell cell = board.getCell(0,0);
-		Set<TestBoardCell> testList = cell.getAdjList();
-		Assert.assertTrue(testList.contains(board.getCell(1, 3)));
+	public void multipleSteps() { 
+		TestBoardCell cell = board.getCell(0, 0);
+		board.calcTargets(cell, 2);
+		Set<TestBoardCell> targets = board.getTargets();
+		Assert.assertTrue(targets.contains(board.getCell(0, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(2, 0)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 1)));	
 	}
-	
-	@Test
-	public void leftEdge() {
-		TestBoardCell cell = board.getCell(0,0);
-		Set<TestBoardCell> testList = cell.getAdjList();
-		Assert.assertTrue(testList.contains(board.getCell(1, 0)));
-	}
-	
-	@Test 
-	public void testOccupied() {
-		TestBoardCell cell = board.getCell(0,0);
-		cell.setOccupied(true);
-		Set<TestBoardCell> testList = cell.getAdjList();
-		Assert.assertEquals(true, cell.isRoom());
-	}
-
 }
