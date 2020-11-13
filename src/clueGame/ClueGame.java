997151dac6623c3e67e9ff7c888e7f12711f42aa
Kenneth Fernandez <kbfernandez@mymail.mines.edu>
2020-11-13 15:58:09 -0800

    Created ClueGame.java skeleton

---
+++ src/clueGame/ClueGame.java
@@ -0,0 +1,12 @@
+package clueGame;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class ClueGame extends JFrame{
+
+	public ClueGame() {
+		JFrame mainGUI = new JFrame();
+	}
+
+}
